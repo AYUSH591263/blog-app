de886e865c47fa8a9407b74283ce60429fd72c41
AYUSH591263 <Ayush2011045@akgec.ac.in>
2023-11-03 05:24:33 +0530

    first commit

---
+++ src/conf/conf.js
@@ -0,0 +1,10 @@
+const conf = {
+    appwriteUrl:String("https://cloud.appwrite.io/v1"),
+    appwriteProjectId:String("653dac44ddc3804b84e9"),
+    appwriteDatabaseId:String("653e76e2742a2bafe8a8"),
+    appwriteCollectionId:String("653e77bf06bda6bdefc9"),
+    appwriteBucketId:String("653e7a6fa8b5363208a2"),
+}
+
+
+export default conf
